f2819ad3cdfb11447ff79b9264b03d4c3325eb14
Alexander Gusakov <alexandergusakov@yahoo.com>
2021-02-05 12:47:23 +0300

    Add Single LinkedList

---
+++ data_structures/LinkedList/SingleLinkedList/LinkedList.py
@@ -0,0 +1,214 @@
+class Node:
+
+	def __init__(self, data=None):
+		self.data = data
+		self.next = None
+		self.is_head = False
+	
+	def __str__(self):
+		return f"{self.data}"
+	
+
+class LinkedList:
+
+	def __init__(self):
+		self.head = Node()
+	
+	
+	def append(self, data) -> None:
+		new_node = Node(data)
+		cur_node = self.head
+		
+		while cur_node.next != None:
+			cur_node = cur_node.next
+		cur_node.next = new_node
+	
+	def display(self, display_type="Vertical"):
+		values: list = self.get_all_values()
+		
+		if len(values) == 0:
+			return "Linked list is empty"
+
+		is_arrow = True
+		for itter_num, value in enumerate(values):
+			if itter_num == len(values) - 1:
+				is_arrow = False
+			
+			node_msg_initial = f"VALUE OF NODE: {value}, NODE INDEX: {itter_num}, "
+			next_node = f"NEXT NODE INDEX: {itter_num + 1}"
+			
+			if is_arrow:
+				node_msg = "[" + node_msg_initial + next_node + "]"
+			else:
+				node_msg = "[" + node_msg_initial + "NEXT NODE INDEX: None" + "]"
+
+			if display_type == "Horizontal":
+				print(node_msg, end="")
+				
+				if is_arrow:
+					print("-->", end="")
+			else:
+				print(node_msg)
+				
+				if is_arrow:
+					print("|".rjust(len(node_msg) // 2))
+					print("|".rjust(len(node_msg) // 2))
+					print("V".rjust(len(node_msg) // 2))
+
+	def length(self):
+		total = 0
+		cur_node = self.head
+		cur_node.is_head = True
+		
+		while True:
+			if cur_node.data:
+				total += 1
+
+			if cur_node.next == None:
+				break
+			cur_node = cur_node.next
+		
+		return total
+	
+	def get_all_values(self) -> list:
+		values = []
+		
+		if self.length() == 0:
+			return values
+
+		cur_node = self.head
+		cur_node.is_head = True
+		
+		while True:
+			if not cur_node.is_head:
+				values.append(cur_node.data)
+				
+				if cur_node.next == None:
+					break
+			cur_node = cur_node.next
+
+		return values
+		
+	def get_value_by_index(self, user_index:int):
+		if (not (user_index >= 0 and user_index < self.length())):
+			raise IndexError("Index out of range")
+		
+		if self.length() == 0:
+			raise Exception("Eror: linked list is empty, there's nothing to get")
+
+		cur_node = self.head
+		inner_index = 0
+		
+		result = None
+		while True:
+			if cur_node.next == None:
+				break
+			cur_node = cur_node.next
+			if inner_index == user_index and cur_node.data:
+				result = cur_node.data
+				break
+			
+			inner_index += 1
+		return result
+	
+	
+	def find_index_with_value(self, user_value):
+		if user_value not in self.get_all_values():
+			raise Exception("Value is not in the linked list")
+	
+		cur_node = self.head
+		inner_index = 0
+
+		while True:
+			if cur_node.next == None:
+				break
+			cur_node = cur_node.next
+
+			if user_value == cur_node.data:
+				return inner_index
+			
+			inner_index += 1
+	
+	def get_node_by_index(self, user_index):
+		if (not (user_index >= 0 and user_index < self.length())):
+			raise IndexError("Index out of range")
+		
+		if self.length() == 0:
+			raise Exception("Eror: linked list is empty, there's nothing to get")
+
+		cur_node = self.head
+		inner_index = 0
+		
+		result = None
+		while True:
+			if cur_node.next == None:
+				break
+			cur_node = cur_node.next
+			if inner_index == user_index and cur_node.data:
+				result = cur_node
+				break
+			
+			inner_index += 1
+		return result
+	
+	def erase_by_index(self, user_index):
+		if (not (user_index >= 0 and user_index < self.length())):
+			raise IndexError("Index out of range")
+		
+		if self.length() == 0:
+			raise Exception("Error: the linked list is empty, nothing to erase")
+	
+		cur_node = self.head
+		cur_node.is_head = True
+		inner_index = 0
+
+		
+		while True:
+			if inner_index == user_index:
+
+				cur_node = self.get_node_by_index(inner_index)
+				if cur_node.next == None and inner_index == 0:
+					self.head.next = None
+
+				elif cur_node.next == None and self.length() - 1 == inner_index:
+					prev_node = self.get_node_by_index(inner_index-1)
+					prev_node.next = None
+				
+				elif cur_node.next != None and inner_index != 0:
+					prev_node = self.get_node_by_index(inner_index - 1)
+					next_node = self.get_node_by_index(inner_index + 1)
+					
+					prev_node.next = next_node
+				
+				elif cur_node.next != None and inner_index == 0:
+					self.head.next = self.get_node_by_index(inner_index + 1)
+				break	
+			inner_index += 1
+
+	def erase_by_value_of_node(self, user_value):
+		if user_value not in self.get_all_values():
+			raise Exception("Value is not in the linked list")
+
+		inner_index = 0
+		cur_node = self.head
+		cur_node.is_head = True
+		while True:
+			cur_node = self.get_node_by_index(inner_index)
+			if cur_node.data == user_value:
+
+				if cur_node.next == None and inner_index == 0:
+					self.head.next = None
+
+				elif cur_node.next == None and self.length() - 1 == inner_index:
+					prev_node = self.get_node_by_index(inner_index-1)
+					prev_node.next = None
+				
+				elif cur_node.next != None and inner_index !=0:
+					prev_node = self.get_node_by_index(inner_index - 1)
+					next_node = self.get_node_by_index(inner_index + 1)
+					
+					prev_node.next = next_node
+				elif cur_node.next != None and inner_index == 0:
+					self.head.next = self.get_node_by_index(inner_index + 1)
+				break	
+			inner_index += 1
---
+++ data_structures/LinkedList/SingleLinkedList/example.py
@@ -0,0 +1,79 @@
+import time
+import random
+from LinkedList import LinkedList
+
+
+TIME_TO_WAIT = 12
+
+new_linked_list = LinkedList()
+
+# Appending new values to the linked list
+print("Appending new values...")
+
+python_list = []
+for i in range(0, 10):
+	random_intger = random.randint(1, 101)
+	python_list.append(random_intger)
+	new_linked_list.append(random_intger)
+
+
+# Getting all values
+values_of_linked_list: list = new_linked_list.get_all_values()
+
+print("Values of the linked list:")
+print(values_of_linked_list)
+
+# Finding the lenght of the linked list
+print("Let's find the length of our linked list")
+time.sleep(TIME_TO_WAIT)
+print(new_linked_list.length())
+
+# Getting value from linked list
+print("Now let's find the value of node with index 4!")
+time.sleep(TIME_TO_WAIT)
+value = new_linked_list.get_value_by_index(4)
+print(value)
+
+# Finding index with value
+print(f"Let's find out the index of {python_list[8]} in the linked list!")
+time.sleep(TIME_TO_WAIT)
+index = new_linked_list.find_index_with_value(python_list[8])
+print(index)
+
+time.sleep(TIME_TO_WAIT)
+print("You also can get node object in memory using the method get_node_by_index")
+
+# Erasing data from the linked list
+time.sleep(TIME_TO_WAIT)
+print("You can erase data from the linked list. There are two methods:\n\
+1)erasing by index\n2)erasing by value.\nLet's try all of them!")
+
+time.sleep(TIME_TO_WAIT)
+
+# Erasing by index
+print(f"Let's erase {new_linked_list.get_value_by_index(0)} using index 0")
+print(new_linked_list.get_all_values())
+new_linked_list.erase_by_index(0)
+print(new_linked_list.get_all_values())
+print("Done")
+
+time.sleep(TIME_TO_WAIT)
+
+print()
+# Erasing by value of node
+index_six_data = new_linked_list.get_node_by_index(6).data
+print(f"Right now we'll use the value of node to erase the node, let's take {index_six_data}")
+
+print(new_linked_list.get_all_values())
+new_linked_list.erase_by_value_of_node(index_six_data)
+print(new_linked_list.get_all_values())
+print("Done")
+
+time.sleep(TIME_TO_WAIT)
+
+print("Now let's display our linked list with full information!")
+time.sleep(TIME_TO_WAIT)
+
+print()
+
+new_linked_list.display()
